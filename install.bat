1c4a4df1e2bbaed6acb5c2ce28c8fe1044fe4cff
jason-bowen-zheng <13239073174@wo.cn>
2020-09-20 21:50:14 +0800

    2020-09-20

--- install.bat
+++ install.bat
@@ -5,7 +5,17 @@ pip install -r requirements.txt >> nul
 echo "[register]"
 python3 register
 echo "[copy files]"
-if not exist %MCPYPATH%\settings.json copy data\json\settings.json /A %MCPYPATH%\settings.json
-mkdir %MCPYPATH%\save
-mkdir %MCPYPATH%\screenshot
+if not exist %MCPYPATH%\settings.json (
+	copy data\json\settings.json /A %MCPYPATH%\settings.json
+)
+if exist %MCPYPATH%\save (
+	echo %%0: %MCPYPATH%\save existed
+) else (
+	mkdir %MCPYPATH%\save
+)
+if exist %MCPYPATH%\screenshot (
+	echo %%0: %MCPYPATH%\screenshot existed
+) else (
+	mkdir %MCPYPATH%\screenshot
+)
 pause
