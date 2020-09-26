fb4c32d32b2ea1c5a5bc07a08d3187ddee6975a2
jason-bowen-zheng <13239073174@wo.cn>
2020-09-26 12:04:57 +0800

    2020-09-26

--- Minecraft/gui/hotbar.py
+++ Minecraft/gui/hotbar.py
@@ -15,16 +15,14 @@ class HotBar(GUI):
         self._element = []
         for i in range(9):
             self._element.append(Sprite(image.load(join(path['texture.ui'], 'hotbar-%d.png' % i)),
-                x=(width - 270) // 2 + i * 30, y=3))
-            self._element[i].scale = 1.5
+                x=(width - 360) // 2 + i * 40, y=5))
+            self._element[i].scale = 2
             self._element[i].opacity = 200
         self.set_index(width, 0)
 
     def draw(self):
         for i in range(len(self._element)):
-            if i == self.index:
-                pass
-            else:
+            if i != self.index:
                 self._element[i].draw()
         else:
             self._element[self.index].draw()
@@ -32,15 +30,18 @@ class HotBar(GUI):
     def set_index(self, width, index):
         for i in range(len(self._element)):
             self._element[i] = Sprite(image.load(join(path['texture.ui'], 'hotbar-%d.png' % i)),
-                x=(width - 270) // 2 + i * 30, y=3)
-            self._element[i].scale = 1.5
+                x=(width - 360) // 2 + i * 40, y=5)
+            self._element[i].scale = 2
             self._element[i].opacity = 200
         if 0 <= index < len(self._element):
             self._element[index] = Sprite(image.load(join(path['texture.ui'], 'hotbar-highlight.png')),
-                    x=(width - 270) // 2 + index * 30 - 1, y=2)
-            self._element[index].scale = 1.5
+                    x=(width - 360) // 2 + index * 40 - 2, y=3)
+            self._element[index].scale = 2
             self.index = index
 
     def resize(self, width, height):
         for i in range(len(self._element)):
-            self._element[i].position = (width - 270) // 2 + i * 30, 3 if i == self.index else 2
+            if i != self.index:
+                self._element[i].position = (width - 360) // 2 + i * 40, 5
+            else:
+                self._element[i].position = (width - 360) // 2 + i * 40 - 2, 3
