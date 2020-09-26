c013190c03c04b2e467ed2ff5c390e3c4209ec55
jason-bowen-zheng <13239073174@wo.cn>
2020-09-26 18:26:29 +0800

    2020-09-26

--- Minecraft/__main__.py
+++ Minecraft/__main__.py
@@ -4,6 +4,15 @@ from Minecraft.utils import *
 
 if __name__ == '__main__':
     if 'MCPYPATH' in environ:
+        try:
+            import psutil
+        except ModuleNotFoundError:
+            pass
+        else:
+            for p in psutil.process_iter():
+                if p.name() == 'python' and p.cmdline()[:3] == ['python', '-m', 'Minecraft']:
+                    log_err('Minecraft process exist, exit')
+                    exit()
         MinecraftLauncher().mainloop()
     else:
         log_err("path 'MCPYPATH' not found")
