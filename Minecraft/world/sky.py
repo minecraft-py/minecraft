8ff2f8d5e05b15ca296bd8967e5652c83a141b7b
jason-bowen-zheng <18917572736@wo.cn>
2020-11-16 21:22:10 +0800

    2020-11-16

--- Minecraft/world/sky.py
+++ Minecraft/world/sky.py
@@ -4,10 +4,14 @@ now = 0
 step = -0.125
 
 def change_sky_color(dt):
+    # 改变天空的颜色
     global now, step
-    get_color_r = lambda x: (x - 11.1111111111111) / -22.2222222222222
-    get_color_g = lambda x: (x - 10.0) / -14.4927536231884
-    get_color_b = lambda x: (x - 11.7647058823529) / -11.7647058823529 
+    # 根据坐标 (0.5, 0) 与 (0.05, 10) 计算出一次函数 x 关于 y 的解析式, 下同
+    get_color_r = lambda y: (y - 11.1111111111111) / -22.2222222222222
+    # (0.69, 0) 与 (0, 10)
+    get_color_g = lambda y: (y - 10.0) / -14.4927536231884
+    # (1, 0) 与 (0.15, 10)
+    get_color_b = lambda y: (y - 11.7647058823529) / -11.7647058823529 
     if now == 10:
         step = -step
     elif now == 0:
@@ -17,3 +21,4 @@ def change_sky_color(dt):
     g = get_color_g(now)
     b = get_color_b(now)
     glClearColor(r, g, b, 1.0)
+    glFogfv(GL_FOG_COLOR, (GLfloat * 4)(r, g, b, 1.0))
