282f35c593b36e94dd4445a9d3ee720c36c34a27
jason-bowen-zheng <18917572736@wo.cn>
2020-11-17 21:55:51 +0800

    2020-11-17

--- Minecraft/gui/xpbar.py
+++ Minecraft/gui/xpbar.py
@@ -16,11 +16,14 @@ class XPBar(GUI):
         self._element = []
         self._start = Sprite(image.load(join(path['texture.ui'], 'xpbar_empty.png')).get_region(0, 0, 12, 5),
                 x=(width - 450) // 2 + 4, y=64)
+        self._start.scale_y = 1.2
         for i in range(39):
             self._element.append(Sprite(image.load(join(path['texture.ui'], 'xpbar_empty.png')).get_region(1, 0, 11, 5),
                 x=(width - 450) // 2 + 12 + 11 * i + 4, y=64))
+            self._element[i].scale_y = 1.2
         self._end = Sprite(image.load(join(path['texture.ui'], 'xpbar_empty.png')).get_region(1, 0, 12, 5),
             x=(width - 450) // 2 + 12 + 11 * 38 + 4, y=64)
+        self._end.scale_y = 1.2
 
     def draw(self):
         self._start.draw()
