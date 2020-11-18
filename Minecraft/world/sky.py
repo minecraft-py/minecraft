d16e23df5d36b4e66c3641d8b7cd17268ceb81b7
jason-bowen-zheng <18917572736@wo.cn>
2020-11-18 20:52:25 +0800

    2020-11-18

--- Minecraft/world/sky.py
+++ Minecraft/world/sky.py
@@ -6,19 +6,25 @@ step = -0.125
 def change_sky_color(dt):
     # 改变天空的颜色
     global now, step
-    # 根据坐标 (0.5, 0) 与 (0.05, 10) 计算出一次函数 x 关于 y 的解析式, 下同
-    get_color_r = lambda y: (y - 11.1111111111111) / -22.2222222222222
-    # (0.69, 0) 与 (0, 10)
-    get_color_g = lambda y: (y - 10.0) / -14.4927536231884
-    # (1, 0) 与 (0.15, 10)
-    get_color_b = lambda y: (y - 11.7647058823529) / -11.7647058823529 
-    if now == 10:
-        step = -step
-    elif now == 0:
+    get_color_r = lambda x: 0.0045 * x ** 2 - 0.09 * x + 0.5
+    get_color_g = lambda x: 0.0069 * x ** 2 - 0.138* x + 0.69
+    get_color_b = lambda x: 0.0085 * x ** 2 - 0.17 * x + 1.0
+    if now == 0 or now == 20:
         step = -step
     now += step
+    print(now, step)
     r = get_color_r(now)
     g = get_color_g(now)
     b = get_color_b(now)
     glClearColor(r, g, b, 1.0)
     glFogfv(GL_FOG_COLOR, (GLfloat * 4)(r, g, b, 1.0))
+
+def get_time():
+    # 获取当前游戏时间
+    global now, step
+    return [now, step]
+
+def set_time(now_time, step_time):
+    # 设置当前游戏时间
+    global now, step
+    now, step = now_time, step_time
