28f64c9dd690efd6c9174b5e1d2c4c455c210646
jason-bowen-zheng <18917572736@wo.cn>
2020-11-23 21:46:39 +0800

    2020-11-23

--- server/source.py
+++ server/source.py
@@ -5,18 +5,6 @@ import time
 
 from server.utils import *
 
-def search_mcpy():
-    # 寻找文件存储位置
-    _os = __import__('os')
-    environ, path = _os.environ, _os.path
-    if 'MCPYPATH' in environ:
-        MCPYPATH = environ['MCPYPATH']
-    elif platform.startswith('win'):
-        MCPYPATH = path.join(path.expanduser('~'), 'mcpy')
-    else:
-        MCPYPATH = path.join(path.expanduser('~'), '.mcpy')
-    return MCPYPATH
-
 path = {}
 path['mcpypath'] = search_mcpy()
 
