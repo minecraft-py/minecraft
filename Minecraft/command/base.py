866f026e7541dfa85b4dbbbaff38c6be9da1af2a
jason-bowen-zheng <18917572736@wo.cn>
2020-12-27 09:40:19 +0800

    2020-12-27

---
+++ Minecraft/command/base.py
@@ -0,0 +1,77 @@
+from shlex import split
+
+from Minecraft.world.block import blocks
+from Minecraft.utils.utils import *
+
+
+class CommandBase():
+    
+    def __init__(self):
+        pass
+
+
+def get_pos(n, s):
+    try:
+        return int(s)
+    except:
+        if s == '~':
+            return n
+        else:
+            if s.startswith('~'):
+                try:
+                    d = int(s[1:])
+                except:
+                    return False
+                else:
+                    return n + d
+            else:
+                return False
+
+def get_args(pos, s, f):
+    l = []
+    px, py, pz = pos
+    command = split(s)[1:]
+    if len(command) != len(f):
+        return False
+    else:
+        for item in range(len(f)):
+            if f[item] == 'block':
+                if command[item] in blocks:
+                    l.append(blocks[command[item]])
+                else:
+                    return False
+            elif f[item] == 'bool':
+                if command[item] == 'true':
+                    l.append(True)
+                elif command[item] == 'false':
+                    l.append(False)
+                else:
+                    return False
+            elif f[item] == 'num':
+                try:
+                    l.append(int(command[item]))
+                except:
+                    return False
+            elif f[item] == 'px':
+                pos = get_pos(px, command[item])
+                if pos is False:
+                    return False
+                else:
+                    l.append(pos)
+            elif f[item] == 'py':
+                pos = get_pos(py, command[item])
+                if pos is False:
+                    return False
+                else:
+                    l.append(pos)
+            elif f[item] == 'pz':
+                pos = get_pos(pz, command[item])
+                if pos is False:
+                    return False
+                else:
+                    l.append(pos)
+            elif f[item] == 'str':
+                l.append(command[item])
+
+        else:
+            return l
