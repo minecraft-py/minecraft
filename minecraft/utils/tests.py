cd1af2376c800e5aa540f15f8c9dc21a438a3a57
jason-bowen-zheng <18917572736@wo.cn>
2021-06-16 06:22:56 +0800

    2021-06-16

--- minecraft/utils/tests.py
+++ minecraft/utils/tests.py
@@ -5,8 +5,9 @@ from minecraft.utils.utils import *
 from pyglet import version
 from pyglet.gl import gl_info
 
-log_info('Start minecraft %s' % VERSION['str'])
+log_info('Start Minecraft-in-python')
+log_info('This is not official Minecraft product. Not approved by or associated with Mojang.')
 log_info('Operation system: %s' % platform)
 log_info('python version: %s' % '.'.join([str(s) for s in version_info[:3]]))
 log_info('pyglet version: %s(OpenGL %s)' % (version, gl_info.get_version()))
-log_info('Minecraft version: %s(data version: %s)' % (VERSION['str'], VERSION['data']))
+log_info('Minecraft-in-python version: %s(data version: %s)' % (VERSION['str'], VERSION['data']))
