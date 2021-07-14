adca38c3865e59a10bed0cbd0a57c15817d7fabe
jason-bowen-zheng <18917572736@wo.cn>
2021-07-14 14:25:43 +0800

    2021-07-14

--- minecraft/client.py
+++ minecraft/client.py
@@ -8,30 +8,29 @@ from minecraft.utils.utils import *
 
 def connect(ip, addr):
     # 创建&连接套接字
-    socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    socket.connect((ip, addr))
-    socket.send('client'.encode())
-    data = socket.recv(1024).decode()
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect((ip, addr))
+    s.send('client'.encode())
+    data = s.recv(1024).decode()
     if data == 'refuse':
         return False
     # 互换版本号
-    else data.startswith('server '):
-        log_info('server version %s' % json.loads(data[7:])['version'])
-        socket.send(('client {"version": %s}' % VERSION['data']).encode())
-        data = socket.recv(1024).decode()
+    elif data.startswith('server '):
+        log_info('server version %s' % json.loads(data[7:])['str'])
+        s.send(('client {"version": %s}' % VERSION['data']).encode())
+        data = s.recv(1024).decode()
         if data == 'refused':
             return False
-        elif data == 'welcome':
-            data = socket.recv(1024).decode()
+        else:
             # 请求玩家名称及 UUID
             if data == 'get_player':
-                socket.send(('player {"id": "%s", "name": "%s"}' % (player['id'], player['name'])).encode())
-                data = socket.recv(1024).decode()
+                s.send(('player {"id": "%s", "name": "%s"}' % (player['id'], player['name'])).encode())
+                data = s.recv(1024).decode()
                 if data == 'refused':
                     return False
                 elif data == 'welcome ' + player['name']:
                     # 验证成功
-                    return socket
+                    return s
 
 def get_info(ip, addr):
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
